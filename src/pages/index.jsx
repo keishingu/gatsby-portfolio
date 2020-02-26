949b7cc932e66cf973e3ac4d45837c5425995ba4
twol <a157523222@gmail.com>
2020-02-26 15:13:44 +0900

    ここまでやった

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <h1>Hi Great Gatsby</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {
+      data.allWorksYaml.edges.map(edge => {
+        const work = edge.node
+        return <div>
+          {work.title} - {work.category} - {work.year}
+        </div>
+      })
+    }
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/about/">Go to about page</Link>
   </Layout>
 )
+
+export const query = graphql`
+    query {
+      allWorksYaml {
+        edges {
+          node {
+            title
+            category
+            year
+          }
+        }
+      }
+    }
+  `
 
 export default IndexPage
