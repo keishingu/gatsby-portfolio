fdfa76ca09bd5ea3d1d1a9e719e363e7ddbd76f3
twol <a157523222@gmail.com>
2020-02-29 23:30:35 +0900

    2.13まで

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const Title = styled.h1`
+    font-size: 24px;
+    color: red;
+`
 
 const AboutPage = () => (
     <Layout>
         <SEO title="About" />
-        <h1>I'm kshingu</h1>
+        <Title>I'm kshingu</Title>
         <Link to="/">Go back to the homepage</Link>
     </Layout>
 )
